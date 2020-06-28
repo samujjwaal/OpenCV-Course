57ce56249d11c51a482ca90c71f26a92ae53043c
Samujjwaal Dey <sdey9@uic.edu>
2020-06-28 18:39:26 -0500

    Add transparent channel to image

--- Basic Image Operations/pixel_manip.py
+++ Basic Image Operations/pixel_manip.py
@@ -6,3 +6,10 @@ color = cv2.imread("butterfly.jpg", 1)
 # convert to grayscale image
 gray = cv2.cvtColor(color, cv2.COLOR_RGB2GRAY)
 cv2.imwrite("gray.jpg", gray)
+
+b = color[:, :, 0]
+g = color[:, :, 1]
+r = color[:, :, 2]
+
+rgba = cv2.merge((b, g, r, g))
+cv2.imwrite("rgba.png", rgba)
