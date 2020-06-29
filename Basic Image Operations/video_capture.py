53d547d0f797eb369a2a3fba8ac9bc581d925903
Samujjwaal Dey <sdey9@uic.edu>
2020-06-28 20:15:11 -0500

    Implement video capture

---
+++ Basic Image Operations/video_capture.py
@@ -0,0 +1,16 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+    cv2.imshow("Frame", frame)
+    # wait for 1 millisecond
+    ch = cv2.waitKey(1)
+    if ch & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
