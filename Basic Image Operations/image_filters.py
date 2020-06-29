05b101867c928bd2fab6a0c11ee91dfcda3eec9e
Samujjwaal Dey <sdey9@uic.edu>
2020-06-28 19:06:52 -0500

    Perform Dilation and Erosion filters

--- Basic Image Operations/image_filters.py
+++ Basic Image Operations/image_filters.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 # import image
 image = cv2.imread("thresh.jpg", 1)
@@ -8,6 +9,15 @@ cv2.imshow("Original", image)
 # blur values for each axis need to be odd numbers
 blur = cv2.GaussianBlur(image, (5, 51), 0)
 cv2.imshow("Blur", blur)
+
+# kernel size for each axis need to be odd numbers
+kernel = np.ones((5, 5), "uint8")
+
+dilate = cv2.dilate(image, kernel, iterations=1)
+erode = cv2.erode(image, kernel, iterations=1)
+
+cv2.imshow("Dilate", dilate)
+cv2.imshow("Erode", erode)
 
 
 cv2.waitKey(0)
