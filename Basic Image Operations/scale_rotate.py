cf8824c8037cc02cbaa4e1cc66ab3f71f587314e
Samujjwaal Dey <sdey9@uic.edu>
2020-06-28 19:56:31 -0500

    Rotate image

--- Basic Image Operations/scale_rotate.py
+++ Basic Image Operations/scale_rotate.py
@@ -11,6 +11,10 @@ cv2.imshow("Half", img_half)
 cv2.imshow("Stretch", img_stretch)
 cv2.imshow("Stretch near", img_stretch_near)
 
+# Rotating image
+M = cv2.getRotationMatrix2D((img.shape[1] / 2, img.shape[0] / 2), -30, 1)
+rotated = cv2.warpAffine(img, M, (img.shape[1], img.shape[0]))
+cv2.imshow("Rotated", rotated)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
