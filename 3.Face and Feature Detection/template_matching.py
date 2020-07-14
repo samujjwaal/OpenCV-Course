4d18b82a3f35c8ef0e5e841337ebe12388fa4a00
Samujjwaal Dey <sdey9@uic.edu>
2020-07-14 17:50:39 -0500

    Implement Template Matching

---
+++ 3.Face and Feature Detection/template_matching.py
@@ -0,0 +1,18 @@
+import cv2
+
+frame = cv2.imread("players.jpg", 0)
+template = cv2.imread("template.jpg", 0)
+
+cv2.imshow("Frame", frame)
+cv2.imshow("Template", template)
+
+result = cv2.matchTemplate(frame, template, cv2.TM_CCOEFF_NORMED)
+
+min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+print(max_val, max_val)
+
+cv2.circle(result, max_loc, 15, 255, 2)
+cv2.imshow("Matching", result)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
