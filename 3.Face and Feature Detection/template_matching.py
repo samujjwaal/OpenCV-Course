d296410e97f7e76331bd11b0580bcd9ba66251e7
Samujjwaal Dey <sdey9@uic.edu>
2020-07-14 21:27:35 -0500

    add comments

--- 3.Face and Feature Detection/template_matching.py
+++ 3.Face and Feature Detection/template_matching.py
@@ -6,11 +6,13 @@ template = cv2.imread("template.jpg", 0)
 cv2.imshow("Frame", frame)
 cv2.imshow("Template", template)
 
+# perform template matching
 result = cv2.matchTemplate(frame, template, cv2.TM_CCOEFF_NORMED)
 
+# find intensity and coordinates of points with lowest and highest intensity
 min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
 print(max_val, max_val)
-
+# highlight brightest spot on image
 cv2.circle(result, max_loc, 15, 255, 2)
 cv2.imshow("Matching", result)
 
